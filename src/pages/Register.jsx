c687d53b5bed44665e317dc6a6507467de8360e8
manshusainishab <manshupallav@gmail.com>
2024-12-06 18:38:16 +0530

    commit

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -28,7 +28,7 @@ const Register = () => {
 
     try {
       const res = await axios.post(
-        "http:localhost:3000/api/v1/user/user/register",
+        "https://stock-pitch-genrator-backend.onrender.com/api/v1/user/user/register",
         {
           firstName,
           lastName,
